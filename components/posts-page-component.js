c6f4ad7cf76f7a61d994fab3382b682add99bacf
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-12-26 19:25:55 +0000

    refactor

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -2,17 +2,10 @@ import { renderPostList } from "./post-list.js";
 import { getPosts } from "../api.js";
 import { loadingContainer } from "./loading-container.js";
 import { renderPage } from "./page.js";
-import { user } from "../auth.js";
-
-//TODO: временно
-const getToken = () => {
-    const token = user ? `Bearer ${user.token}` : undefined;
-    return token;
-};
 
 export function renderPostsPageComponent() {
     const loadingPosts = loadingContainer(
-        getPosts({ token: getToken() }).then(posts => renderPostList(posts))
+        getPosts().then(posts => renderPostList(posts))
     );
 
     const element = renderPage(loadingPosts);
