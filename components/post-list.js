ad3fd23a0207637e9dc69c90fbd81e0b1c47b8e0
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-12-28 23:29:56 +0000

    sort

--- components/post-list.js
+++ components/post-list.js
@@ -2,9 +2,17 @@ import { renderApp } from "../index.js";
 import { renederPost } from "./post.js";
 import { fromHTML } from "./render.js";
 
+const sortFunctions = {
+    newest: (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+    oldest: (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+    popular: (a, b) => b.likes.length - a.likes.length
+};
+
 export const renderPostList = posts => {
-    let sortOrder = 0;
+    let sortOrder = sortFunctions.newest;
     let dateFilter = null;
+
+   
 
     const onLikeChaged = post => {
         const index = posts.findIndex(p => p.id === post.id);
@@ -15,8 +23,30 @@ export const renderPostList = posts => {
     };
 
     return () => {
-        const element = fromHTML(`<ul class="posts"/>`);
-        element.append(...posts.map(post => renederPost(post, onLikeChaged)));
+        const element = fromHTML(`
+            <div>
+                <div class="post-list__control-panel">                    
+                    <button class="link-button sort-button ${sortOrder === sortFunctions.newest ? 'active-option' : ''}" data-sort="newest">новые</button>
+                    <button class="link-button sort-button ${sortOrder === sortFunctions.oldest ? 'active-option' : ''}" data-sort="oldest">старые</button>
+                    <button class="link-button sort-button ${sortOrder === sortFunctions.popular ? 'active-option' : ''}" data-sort="popular">популярные</button>
+                </div>
+                <ul class="posts"/>
+            </div>
+        `);
+        
+        for(let button of element.querySelectorAll('.sort-button')){
+            button.addEventListener('click', event => {            
+                sortOrder = sortFunctions[event.target.dataset.sort];
+                renderApp();
+            });
+        }
+
+        element.querySelector('.posts').append(
+            ...posts
+            .sort(sortOrder)
+            .map(post => renederPost(post, onLikeChaged))
+        );
+
         return element;
     };
 };
