e56ab24f0db7948f6334641fefca3f3af4054e03
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-12-30 21:43:33 +0000

    format date locale

--- components/post.js
+++ components/post.js
@@ -4,6 +4,7 @@ import { goToPage } from "../index.js";
 import { USER_POSTS_PAGE } from "../routes.js";
 import { fromHTML } from "./utils.js";
 import { formatDistanceToNow } from 'date-fns'
+import ru from "date-fns/locale/ru";
 
 export const renederPost = ({post, onPostChanged, withHeader = true}) => {
     const likeImage = `./assets/images/${post.isLiked ? "like-active.svg" : "like-not-active.svg"}`;
@@ -13,7 +14,7 @@ export const renederPost = ({post, onPostChanged, withHeader = true}) => {
             ${withHeader 
                 ? `<div class="post-header">
                         <img src="${post.user.imageUrl}" class="post-header__user-image">
-                        <p class="post-header__user-name">${post.user.name}</p>
+                        <p class="post-header__user-name data-user-name"></p>
                     </div>`
                 : ``
             }            
@@ -29,14 +30,18 @@ export const renederPost = ({post, onPostChanged, withHeader = true}) => {
                 </p>
             </div>
             <p class="post-text">
-                <span class="user-name">${post.user.name}</span>
-                ${post.description}
+                <span class="user-name data-user-name"></span>
+                <span class="data-post-description"></span>
+                
             </p>
             <p class="post-date">
-                ${ formatDistanceToNow(post.createdAt)}
+                ${ formatDistanceToNow(post.createdAt, {locale: ru})}
             </p>
         </li>`);
 
+    //against the html injection atack
+    for (let e of element.querySelectorAll('.data-user-name')) e.textContent = post.user.name;
+    for (let e of element.querySelectorAll('.data-post-description')) e.textContent = post.description;
     
     element.querySelector(".like-button").addEventListener("click", (event) => {
         if(!userState.get()){
